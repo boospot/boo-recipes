0bc9953960811962aaaf8bb7301e5689acf795ab
BooSpot <boospotmanager@gmail.com>
2018-12-06 14:58:34 +0500

    updated uninstall function to completely remove everything related to plugin

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,7 @@ $plugin_option_id         = 'boorecipe-options';
 $plugin_widgets_option_id = 'boorecipe-registered-shortcodes';
 $plugin_meta_key          = 'boorecipe-recipe-meta';
 
+$current_language_code = mb_substr( get_locale(), 0, 2 );;
 
 $plugin_options = get_option( $plugin_option_id );
 
@@ -18,60 +19,77 @@ if ( ! empty( $plugin_options ) ) {
 	/*
 	 * Delete options if options say so
 	 */
-	$uninstall_delete_options = isset( $plugin_options['uninstall_delete_options'] ) ? $plugin_options['uninstall_delete_options'] : 'yes';
+	$uninstall_delete_options = isset( $plugin_options[ $current_language_code ]['uninstall_delete_options'] ) ? $plugin_options[ $current_language_code ]['uninstall_delete_options'] : 'yes';
 
 	if ( $uninstall_delete_options === 'yes' ) {
 		delete_option( $plugin_option_id );
 		delete_option( $plugin_widgets_option_id );
 	}
 
-
-	/*
-	 * Delete recipe meta data if options say so
-	 */
-
-	$uninstall_delete_meta = isset( $plugin_options['uninstall_delete_meta'] ) ? $plugin_options['uninstall_delete_meta'] : 'no';
-
-	if ( $uninstall_delete_meta === 'yes' ) {
+}
 
 
-		$all_recipes = get_posts( 'numberposts=-1&post_type=boo_recipe&post_status=any' );
+/*
+ * Delete recipe meta data if options say so
+ */
 
-		// delete all recipes
-		foreach ( $all_recipes as $recipe ) {
-			wp_delete_post( $recipe->ID, true );
+$uninstall_delete_meta = isset( $plugin_options[ $current_language_code ]['uninstall_delete_meta'] ) ? $plugin_options[ $current_language_code ]['uninstall_delete_meta'] : 'no';
+
+if ( $uninstall_delete_meta === 'yes' ) {
+
+
+	$all_recipes = get_posts( 'numberposts=-1&post_type=boo_recipe&post_status=any' );
+
+	foreach ( $all_recipes as $recipe ) {
+
+		$meta_values = get_post_meta( $recipe->ID );
+		foreach ( $meta_values as $key => $val ) {
+			delete_post_meta( $recipe->ID, $key );
 		}
 
+		wp_delete_post( $recipe->ID, true );
 
-		// delete all postmeta
-		$meta_keys = Boorecipe_Globals::get_meta_fields();
-		foreach ( $meta_keys as $meta_key ) {
-			delete_post_meta_by_key( $meta_key );
-		}
 
-		// delete all terms of taxonomies
-		$recipe_taxonomies = array(
-			'recipe_category',
-			'recipe_cuisine',
-			'skill_level',
-			'recipe_tags',
-			'cooking_method'
-		);
-		foreach ( $recipe_taxonomies as $taxonomy ) {
-			$terms = get_terms( $taxonomy, array( 'fields' => 'ids', 'hide_empty' => false ) );
-
-			if ( is_array( $terms ) && ! empty( $terms ) ) {
-				foreach ( $terms as $term_id ) {
-					wp_delete_term( $term_id, $taxonomy );
-				}
-			}
-		}
-
+//		wp_delete_object_term_relationships( $recipe->ID, $recipe_taxonomies );
 
 	}
+
+
+
+// delete all terms of taxonomies
+	$recipe_taxonomies = array(
+		'recipe_category',
+		'recipe_cuisine',
+		'skill_level',
+		'recipe_tags',
+		'cooking_method'
+	);
+
+
+	foreach ( $recipe_taxonomies as $taxonomy ) {
+		global $wpdb;
+
+		$query = 'SELECT t.name, t.term_id
+            FROM ' . $wpdb->terms . ' AS t
+            INNER JOIN ' . $wpdb->term_taxonomy . ' AS tt
+            ON t.term_id = tt.term_id
+            WHERE tt.taxonomy = "' . $taxonomy . '"';
+
+		$terms = $wpdb->get_results($query);
+
+		foreach ($terms as $term) {
+			wp_delete_term( $term->term_id, $taxonomy );
+		}
+
+		wp_reset_query();
+
+	}
+
+
+
 }
 
 
 
 
-// Bue Bye, Good Luck!
+// Bye Bye, Good Luck!
