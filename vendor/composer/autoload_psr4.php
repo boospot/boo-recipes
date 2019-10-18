4bc0c27a3735514cd2be9a191c0bfb24f077ece0
Rao Abid <rao@booskills.com>
2019-10-18 15:37:19 +0500

    admin notices helper added

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'WPTRT\\AdminNotices\\' => array($vendorDir . '/wptrt/admin-notices/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,9 +10,25 @@ class ComposerStaticInitd390322b992c68fd2c3d035a03d13b5b
         '8ca8a91f0a826d6c6c8f274c90ca7d88' => __DIR__ . '/..' . '/wpmetabox/meta-box/meta-box.php',
     );
 
+    public static $prefixLengthsPsr4 = array (
+        'W' => 
+        array (
+            'WPTRT\\AdminNotices\\' => 19,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'WPTRT\\AdminNotices\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/wptrt/admin-notices/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitd390322b992c68fd2c3d035a03d13b5b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitd390322b992c68fd2c3d035a03d13b5b::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
